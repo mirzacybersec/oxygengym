76cdd5f8edc52597391a43d03ee176dcaae7bd8d
mirzacybersec <116951193+mirzacybersec@users.noreply.github.com>
2026-06-28 15:21:37 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/oxygengym/',  // ← ADD THIS LINE
+  base: '/',  // ← ADD THIS LINE
   plugins: [
     react(),
   ],
